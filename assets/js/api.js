f3126f06e00ff9ac7a87cf75c716de707241ce57
Bruno Pitman <62653706+pitmania@users.noreply.github.com>
2023-06-03 16:22:53 -0300

    initial commit

---
+++ assets/js/api.js
@@ -0,0 +1,7 @@
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/pitmania/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
